e069cf103ccd8b7d9f78d4c2c92a59b1b0a5369f
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-07 11:07:41 -0800

    Initialized node, made children method

--- W4D1/skeleton/lib/tic_tac_toe_node.rb
+++ W4D1/skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,18 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    moves = []
+    @board.rows.each_with_index do |row, idx1|
+      row.each_with_index do |ele, idx2|
+        pos = [idx1, idx2]
+        if @board.empty?(pos)
+          test_board = @board.dup
+          test_board[pos] = @next_mover_mark
+          next_mark = @next_mover_mark == :x ? :o : :x
+          moves << TicTacToeNode.new(test_board, next_mark, pos)
+        end
+      end
+    end
+    moves
   end
 end
