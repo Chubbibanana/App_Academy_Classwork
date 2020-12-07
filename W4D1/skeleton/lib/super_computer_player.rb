94e7b95e8e91728ba6bc518e2fce7239bcc57ed5
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-07 11:40:09 -0800

    Created class supercomputerplayer and finished AI functionalities

--- W4D1/skeleton/lib/super_computer_player.rb
+++ W4D1/skeleton/lib/super_computer_player.rb
@@ -2,7 +2,11 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
-    @game = TicTacToeNode.new(game.board, mark)
+    new_node = TicTacToeNode.new(game.board, mark)
+    children = new_node.children
+    children.each {|child| return child.prev_move_pos if child.winning_node?(mark)}
+    children.each {|child| return child.prev_move_pos unless child.losing_node?(mark)}
+    raise 'Invalid game state'
   end
 end
 
