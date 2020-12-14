1f5dce2a0ddf9cf09c6c2277bebfe7630b4cf3a7
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-14 14:15:26 -0800

    Did p02_hashing.rb

--- w5d1/skeleton 3/lib/p02_hashing.rb
+++ w5d1/skeleton 3/lib/p02_hashing.rb
@@ -4,11 +4,28 @@ end
 
 class Array
   def hash
+    crypt = 0
+    self.each_with_index do |ele, i|
+        if ele.is_a?(Integer)
+          crypt += ele.hash * i
+        elsif ele.is_a?(String)
+          crypt += ele.hash * i
+        elsif ele.is_a?(Array)
+          count = 3
+          crypt += count + ele.hash 
+        end
+    end
+    crypt *= self.length + 1
   end
 end
 
 class String
   def hash
+    crypt = 0
+    self.each_char.with_index do |char, i|
+      crypt += char.ord ** i
+    end
+    crypt *= self.length + 1
   end
 end
 
@@ -16,6 +33,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.hash * 1000 + self.values.hash * 29
   end
 end
