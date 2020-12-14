3325dce6df661182d3aa2b10128802b5046fb66a
Edwin Cheng <edwincheng3@gmail.com>
2020-12-14 15:44:32 -0800

    Working on Part 5 - Hash Map

--- w5d1/skeleton 3/lib/p05_hash_map.rb
+++ w5d1/skeleton 3/lib/p05_hash_map.rb
@@ -12,20 +12,53 @@ class HashMap
   end
 
   def set(key, val)
-    
+    index = bucket(key)
+    if @store[index].include?(key)
+      @store[index].update(key, val)
+    else
+      @store[index].append(key, val)
+      count += 1
+    end
   end
 
   def get(key)
+    index = bucket(key)
+    if @store[index].include?(key)
+      @store[index].get(key)
+    else
+      nil
+    end
   end
 
   def delete(key)
+    index = bucket(key)
+    @store[index].remove(key)
   end
 
-  # def each(&prc)
-  #   @store.each do |linked_list|
-  #     linked_list.each(&prc)
-  #   end
-  # end
+  def []=(key, val)
+    index = bucket(key)
+    if @store[index].include?(key)
+      @store[index].update(key, val)
+    else
+      @store[index].append(key, val)
+      count += 1
+    end
+  end
+
+  def [](key)
+    index = bucket(key)
+    if @store[index].include?(key)
+      @store[index].get(key)
+    else
+      nil
+    end
+  end
+
+  def each(&prc)
+    @store.each do |linked_list|
+      prc.call(linked_list)
+    end
+  end
 
   # uncomment when you have Enumerable included
   # def to_s
@@ -49,5 +82,6 @@ class HashMap
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    key.hash % num_buckets
   end
 end
