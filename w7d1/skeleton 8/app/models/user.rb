7cd48d051800c9f5c4878d5ec06a91f1a1ceb0c7
JCS <joshsad2@gmail.com>
2021-01-04 14:21:31 -0800

    working on sessions controller

--- w7d1/skeleton 8/app/models/user.rb
+++ w7d1/skeleton 8/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, :session_token, presence: true, uniqueness: true
+    validates :user_name, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
 
     attr_reader :password
@@ -16,4 +16,18 @@ class User < ApplicationRecord
         # password is hashed and salted by BCrypt
     end
 
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+        if user && user.is_password?(password)
+          user
+        else
+          nil
+        end
+    end
+
+    
 end
