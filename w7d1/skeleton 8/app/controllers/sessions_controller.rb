a37ebfffcc5dee46c6c0f8047f755a4955584950
Chubbibanana <dongsoo.tchah@gmail.com>
2021-01-04 14:38:39 -0800

    Finished app_controller and sessions_controller

--- w7d1/skeleton 8/app/controllers/sessions_controller.rb
+++ w7d1/skeleton 8/app/controllers/sessions_controller.rb
@@ -5,4 +5,26 @@ class SessionsController < ApplicationController
     render :new
   end
 
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:user_name]
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user) # write this method
+      # @user.reset_session_token!
+      redirect_to cats_url
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    # user = User.find_by(id: params[:id])
+    # user.destroy
+    # render json: user
+    logout(user)
+
+  end
 end
