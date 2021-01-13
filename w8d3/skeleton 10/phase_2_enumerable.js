242dd2bd7294326e0e9f6e1e5e2508469d4bdb3e
colewendling <colewendling@utexas.edu>
2021-01-13 17:06:14 -0600

    add myMap monkey-patch in JS:29

--- w8d3/skeleton 10/phase_2_enumerable.js
+++ w8d3/skeleton 10/phase_2_enumerable.js
@@ -8,13 +8,22 @@ Array.prototype.myEach = function (myfunc) {
     }
 };
 
-[1, 2, 3, 4].myEach(num => {   
+[1, 2, 3, 4].myEach(num =>    
     console.log(num)
-});
+);
 
 
 // Array#myMap monkey-patch
 
 Array.prototype.myMap = function (myfunc) {
-    
-}
+    const mappedArray = [];
+
+    this.myEach(el => mappedArray.push(myfunc(el)) );
+    return mappedArray;
+};
+
+
+console.log([1, 2, 3, 4].myMap(num => num * 2 ));
+
+
+// Array#myInject monkey-patch
