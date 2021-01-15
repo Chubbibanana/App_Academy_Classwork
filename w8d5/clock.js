beab06d0881f3fd8997adfa4f18082df7b23148b
shinara03 <shinara03@gmail.com>
2021-01-15 12:22:37 -0800

    finish timing is everything part

--- w8d5/clock.js
+++ w8d5/clock.js
@@ -2,21 +2,31 @@ class Clock {
     constructor() {
         
         // 1. Create a Date object.
-        let date = new Date();
+        const timeNow = new Date();
+
         // 2. Store the hours, minutes, and seconds.
-        let hours 
+        this.hours = timeNow.getHours();
+        this.minutes = timeNow.getMinutes();
+        this.seconds = timeNow.getSeconds();
+
         // 3. Call printTime.
+        this.printTime();
         // 4. Schedule the tick at 1 second intervals.
+        setInterval(this._tick.bind(this),1000);
     }
 
     printTime() {
         // Format the time in HH:MM:SS
         // Use console.log to print it.
+        const time = [this.hours, this.minutes, this.seconds].join(":");
+        console.log(time);
     }
 
     _tick() {
         // 1. Increment the time by one second.
         // 2. Call printTime.
+        this.seconds += 1;
+        this.printTime();
     }
 }
 
