08bf1fdb985dd63a2440b8c495b9e710a8817e94
Chubbibanana <dongsoo.tchah@gmail.com>
2021-01-25 15:29:22 -0800

    Finished phase 1

--- w10d1/src/dom_node_collection.js
+++ w10d1/src/dom_node_collection.js
@@ -42,6 +42,62 @@ class DOMNodeCollection {
         }
     };
 
+    attr(attribute, value) {
+        if (typeof value === 'string') {
+            this.HTMLElements.forEach(el => {
+                el.setAttribute(attribute, value);
+            })
+        } else {
+            return this.HTMLElements[0].getAttribute(attribute);
+        }
+    }
+
+    addClass(...newClasses) {
+        this.HTMLElements.forEach(el => {
+            newClasses.forEach(newclass => {
+                el.classList.add(newclass);
+            })
+        })
+    }
+
+    removeClass(...newClasses) {
+        this.HTMLElements.forEach(el => {
+            newClasses.forEach(newclass => {
+                el.classList.remove(newclass);
+            })
+        })
+    }
+
+    children() {
+        let childs = [];
+        this.HTMLElements.forEach(el => {
+            childs = childs.concat(Array.from(el.children));
+        })
+        return new DOMNodeCollection(childs);
+    }
+
+    parent() {
+        let parents = [];
+        this.HTMLElements.forEach(el => {
+            if (!parents.includes(el.parentElement)) parents.push(el.parentElement);
+        })
+        return new DOMNodeCollection(parents);
+    }
+
+    find(selector) {
+        let found = [];
+        this.HTMLElements.forEach(el => {
+            const nodeList = el.querySelectorAll(selector);
+            found = found.concat(Array.from(nodeList));
+        })
+        return new DOMNodeCollection(found);
+    }
+
+    remove() {
+        this.HTMLElements.forEach(el => {
+            el.innerHTML = ' ';
+        })
+    }
 }
 
 module.exports = DOMNodeCollection;
