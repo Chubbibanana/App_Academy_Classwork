da3d7ce903ac39bcd7845f9a9984322261e618aa
Tului <tgantulga@ucdavis.edu>
2021-01-25 14:44:12 -0800

    finished append

--- w10d1/src/dom_node_collection.js
+++ w10d1/src/dom_node_collection.js
@@ -11,17 +11,36 @@ class DOMNodeCollection {
         })} else {
             return this.HTMLElements[0].innerHTML;
         }
-    }
+    };
 
     empty() {
-        this.html('');
-    }
+        this.html(' ');
+    };
 
-    append(outer) {
-        this.HTMLElements.forEach(element => {
-            this.html(outer);
-        })
-    }
+    append(inp) {
+        // debugger
+        if (typeof inp === 'string'){
+            this.HTMLElements.forEach(element => {
+                element.innerHTML += inp;
+            })
+        } else if (inp instanceof Array && inp.every(el => el instanceof HTMLElement)){
+            this.HTMLElements.forEach(element => {
+                inp.forEach(el => {
+                    element.innerHTML += el.outerHTML;
+                })
+            })
+        } else if (inp instanceof HTMLElement) {
+            this.HTMLElements.forEach(element => {
+                element.innerHTML += inp.outerHTML;
+            })
+        } else if (inp instanceof DOMNodeCollection) {
+            this.HTMLElements.forEach(element => {
+                inp.HTMLElements.forEach(el2 =>{
+                    element.innerHTML += el2.outerHTML;
+                })
+            })
+        }
+    };
 
 }
 
