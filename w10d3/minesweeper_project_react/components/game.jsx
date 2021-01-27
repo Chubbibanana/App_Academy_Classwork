c9c934747f78df5b6b2cd99f03d9c4d4fc756390
Chubbibanana <dongsoo.tchah@gmail.com>
2021-01-27 15:29:36 -0800

    Everything but reset button working now

--- w10d3/minesweeper_project_react/components/game.jsx
+++ w10d3/minesweeper_project_react/components/game.jsx
@@ -10,19 +10,54 @@ class Game extends React.Component{
             board: board
         };
         this.updateGame = this.updateGame.bind(this);
-
+        this.renderModal = this.renderModal.bind(this);
+        this.restartGame = this.restartGame.bind(this);
     }
 
-    updateGame() {
+    updateGame(tile, isFlagged) {
+        if (isFlagged) {
+            tile.toggleFlag();
+        } else {
+            tile.explore();
+        }
 
+        this.setState({ board: this.state.board });
     }
 
     render() {
+        let modal = this.renderModal();
         return (
             <div>
+                {modal}
                 <Board board={this.state.board} updateGame={this.updateGame} />
             </div>
         )
+    }
+
+    renderModal() {
+        let message;
+        if (this.state.board.lost()) {
+            message = 'you have lost';
+        } else if (this.state.board.won()) {
+            message = 'you have won';
+        }
+        if (message) {
+            return (
+                <div className='modal-screen'>
+                    <div className='modal-content'>
+                        <p>{message}</p>
+                        <button onClick={this.restartGame}>Play Again!!!</button>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    restartGame() {
+        const board = new Minesweeper.Board(9, 9);
+        this.setState = {
+            board: board
+        };
     }
 }
 
--- w10d3/minesweeper_project_react/components/tile.jsx
+++ w10d3/minesweeper_project_react/components/tile.jsx
@@ -3,25 +3,44 @@ import React from 'react';
 class Tile extends React.Component{
     constructor(props){
         super(props);
-
+        this.handleClick = this.handleClick.bind(this);
     }
     
     render() {
         const tile = this.props.tile;
-        let tileString = 'u';
+        let tileString;
+        let cssClass;
 
-        if (tile.bombed) {
-            tileString = '\u1F4A3';
-        } else if (tile.explored) {
-            tileString = 'e';
+        if (tile.explored) {
+            if (tile.bombed) {
+                cssClass = 'bombed';
+                tileString = 'b';
+            }
+            else {
+                cssClass = 'explored';
+                const adjacents = tile.adjacentBombCount();
+                adjacents === 0 ? (tileString = '') : (tileString = `${adjacents}`);
+                
+                // tile.explore();
+            }
+
         } else if (tile.flagged) {
+            cssClass = 'flagged';
             tileString = '\u2691';
         }
+        cssClass = `tile ${cssClass}`;
         return (
-            <div className='tile'>
-                <button>{tileString}</button>
+            <div className={cssClass} onClick={this.handleClick}>
+                {tileString}
             </div>
-        )};
+        );
+    }
+
+    handleClick(e) {
+        let isFlagged = false;
+        if (e.altKey) isFlagged = true;
+        this.props.updateGame(this.props.tile, isFlagged);
+    }
 }
 
 export default Tile;
