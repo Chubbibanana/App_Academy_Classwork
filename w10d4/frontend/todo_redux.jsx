a488acd4846accc2e64b3be984cbf714ea82f550
Chubbibanana <dongsoo.tchah@gmail.com>
2021-01-30 12:16:28 -0800

    Setup store, and some actions/reducers

--- w10d4/frontend/todo_redux.jsx
+++ w10d4/frontend/todo_redux.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.store = configureStore; // set window store prop to default store;
     ReactDOM.render(<h1>Todo App</h1>, document.getElementById("content"));
 })
